abaa0422825e5453d72bb825ef9c189760ecbb04
ChrisRome00 <romero.chrisprof@gmail.com>
2024-07-14 22:27:50 -0500

    Added Modal and function for creating task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,34 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    const taskID = dayjs().valueOf();
+    //console.log(taskID);
+    return taskID;
 }
 
 // Todo: create a function to create a task card
+// given a task
 function createTaskCard(task) {
+    // Create all task card elements
+    const taskCard = $('<div>');
+    const taskCardHeader = $('<div>');
+    const taskCardHeaderTitle = $('<h1>');
+    const taskCardBody = $('<div>');
+    const taskCardDescription = $('<p>');
+    const taskCardDate = $('<p>');
+    const taskCardDeleteBtn = $('<a>');
 
+    // Store content in task card
+    taskCard.append(taskCardHeader);
+    taskCard.append(taskCardBody);
+
+    // Store content in respected sections
+    taskCardHeader.append(taskCardHeaderTitle);
+    taskCardBody.append(taskCardDescription);
+    taskCardBody.append(taskCardDate);
+    taskCardBody.append(taskCardDeleteBtn);
+
+    // 
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -19,7 +41,7 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    
 }
 
 // Todo: create a function to handle deleting a task
@@ -32,7 +54,8 @@ function handleDrop(event, ui) {
 
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// Todo: 
+// when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+   const taskList = 
 });
