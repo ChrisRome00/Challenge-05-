bbed5fc0101d7a7c154ae5e8a2587dabeab1bb0f
ChrisRome00 <romero.chrisprof@gmail.com>
2024-07-16 01:17:17 -0500

    Adjusted html to match modal in example, and added some JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,22 +25,49 @@ function createTaskCard(task) {
     taskCard.append(taskCardHeader);
     taskCard.append(taskCardBody);
 
-    // Store content in respected sections
+    // Store content in respected sections of the task card
     taskCardHeader.append(taskCardHeaderTitle);
     taskCardBody.append(taskCardDescription);
     taskCardBody.append(taskCardDate);
     taskCardBody.append(taskCardDeleteBtn);
 
-    // 
+    // Add information to cards
+    taskCardHeaderTitle.text(task.taskTitle);
+    taskCardDescription.text(task.taskDescription);
+    taskCardDate.text(task.date);
+    taskCardDeleteBtn.text('Delete');
+
+    /* Style card depending on status */
+    const today = dayjs();
+    const due = task.taskDueDate;
+
+
+    if (today.isAfter(dayjs(due)) )
+
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
+
+    const toDoListSection = $('#todo-cards');
+    const inProgressListSection = $('#in-progress-cards');
+    const doneListSection = $('#done-cards');
+
+    //make them
+    toDoListSection.addClass('connectedSortable h-100');
+    inProgressListSection.addClass('connectedSortable h-100');
+    doneListSection.addClass('connectedSortable h-100');
+
+
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+
+    const taskTitle = $('#taskTitle').val();
+    const dayChose = $('#datepicker').val()
+    const taskDueDate = dayjs(dayChose).format('MM/DD/YYYY');
     
 }
 
@@ -57,5 +84,17 @@ function handleDrop(event, ui) {
 // Todo: 
 // when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-   const taskList = 
+
+    //Check if local storage is available to take from or empty, if empty create its array
+    taskList = taskList || [];
+    nextId = nextId || [];
+   
+    renderTaskList();
+
+    $( "#datepicker" ).datepicker({
+        changeMonth: true,
+        changeYear: true
+      });
+
+
 });
